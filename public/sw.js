ce4965bb22499a2f3d502148e54b3aa79235584b
hvkst <bernd@hvkst.de>
2022-10-15 20:36:13 +0200

    cyclicUpdating

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,9 @@
+// install service worker
 self.addEventListener('install', (evt) => {
   console.log('service worker has been installed');
+});
+
+// activate service worker
+self.addEventListener('activate', (evt) => {
+  console.log('service worker has been activated');
 });
