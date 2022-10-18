a0de287feb8b7e7d290e009eeb076571f2f31abf
hvkst <bernd@hvkst.de>
2022-10-19 00:03:25 +0200

    add darkMode

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,9 +5,6 @@ const router = require('express').Router();
 /* GET home page */
 router.get('/', isDarkmode, (req, res, next) => {
   const darkmode = req.darkmode;
-  // if (req.isAuthenticated()) {
-  //   return res.redirect('/dashboard');
-  // }
   res.render('index', { darkmode });
 });
 
